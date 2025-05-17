5fcf59d7b90fc38b927398c7ea7678909c9aaf16
Rahmat Eka Marup <male.malice@gmail.com>
2025-05-17 18:24:07 +0700

    reset database

--- backend/prisma/migrations/20250515094007_add_risk_assessment_item_relations/migration.sql
+++
@@ -1,5 +0,0 @@
--- AddForeignKey
-ALTER TABLE "t_risk_assessment_item" ADD CONSTRAINT "t_risk_assessment_item_mThreatId_fkey" FOREIGN KEY ("mThreatId") REFERENCES "m_threats"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "t_risk_assessment_item" ADD CONSTRAINT "t_risk_assessment_item_mHseCategoryId_fkey" FOREIGN KEY ("mHseCategoryId") REFERENCES "m_hse_categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- backend/prisma/migrations/20250516023018_alter_risk_assessment_add_action_plan/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "t_risk_assessment" ADD COLUMN     "action_plan" TEXT,
-ADD COLUMN     "assignee_id" TEXT;
