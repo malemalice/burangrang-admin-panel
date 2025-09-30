a082d6292af20d0bf13a14d02716940dfe9602b3
Rahmat Eka <male.malice@gmail.com>
2025-09-30 17:28:28 +0700

    fix serializer issues

--- backend/src/modules/roles/dto/role.dto.ts
+++ backend/src/modules/roles/dto/role.dto.ts
@@ -1,12 +1,26 @@
+import { Expose } from 'class-transformer';
 import { PermissionDto } from '../../../modules/permissions/dto/permission.dto';
 
 export class RoleDto {
+  @Expose()
   id: string;
+
+  @Expose()
   name: string;
+
+  @Expose()
   description?: string | null;
+
+  @Expose()
   isActive: boolean;
+
+  @Expose()
   permissions?: PermissionDto[];
+
+  @Expose()
   createdAt: Date;
+
+  @Expose()
   updatedAt: Date;
 
   constructor(partial: Partial<RoleDto>) {
