a85124a289a63dfa450020233c8df2f45549f808
Rahmat Eka Marup <male.malice@gmail.com>
2026-01-16 17:27:40 +0700

    fix migration for approval item relations

--- backend/prisma/migrations/20260116092617_add_status_inspection_item/migration.sql
+++ backend/prisma/migrations/20260116092617_add_status_inspection_item/migration.sql
@@ -3,9 +3,3 @@ ALTER TABLE "t_inspection_items" ADD COLUMN     "status" "GeneralStatusEnum" NOT
 
 -- CreateIndex
 CREATE INDEX "t_inspection_items_status_idx" ON "t_inspection_items"("status");
-
--- AddForeignKey
-ALTER TABLE "m_approval_item" ADD CONSTRAINT "m_approval_item_jobPositionId_fkey" FOREIGN KEY ("jobPositionId") REFERENCES "m_job_positions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "m_approval_item" ADD CONSTRAINT "m_approval_item_departmentId_fkey" FOREIGN KEY ("departmentId") REFERENCES "m_departments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
