21147ee8fee3be3e36dd2ae1873fe8537097099c
Rahmat Eka Marup <male.malice@gmail.com>
2026-02-10 13:47:32 +0700

    fix invalid import

--- backend/src/modules/user-permissions/user-permissions.module.ts
+++ backend/src/modules/user-permissions/user-permissions.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserPermissionsController } from './user-permissions.controller';
 import { UserPermissionsService } from './user-permissions.service';
-import { PrismaModule } from '../../../core/modules/prisma.module';
-import { SharedModule } from '../../../shared/shared.module';
+import { PrismaModule } from '../../core/prisma/prisma.module';
+import { SharedModule } from '../../shared/shared.module';
 
 @Module({
   imports: [PrismaModule, SharedModule],
--- backend/src/modules/user-permissions/user-permissions.service.ts
+++ backend/src/modules/user-permissions/user-permissions.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../../../core/services/prisma.service';
-import { ErrorHandlingService } from '../../../shared/services/error-handling.service';
+import { PrismaService } from '../../core/prisma/prisma.service';
+import { ErrorHandlingService } from '../../shared/services/error-handling.service';
 
 @Injectable()
 export class UserPermissionsService {
